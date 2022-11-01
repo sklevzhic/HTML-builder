d2a04dc8aa4ff08213fbbe06b02f136aaab64b37
sklevzhic <sklevzhic@gmail.com>
2022-11-01 23:28:58 +0300

    Update screenshot in Read.me feat: add method for get files in folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const {mkdir, readdir, copyFile, unlink} = require('node:fs/promises');
+const path = require('path');
+
+copyFolder()
+
+async function copyFolder() {
+	const folderCopy = await readdir(__dirname);
+	if (folderCopy.includes("files-copy")) {
+		const filesOld = await readdir("files-copy");
+		for (const fileOld of filesOld) {
+			await unlink(path.join("files-copy", fileOld))
+		}
+	} else {
+		await mkdir("files-copy", { recursive: true });
+	}
+
+	const files = await readdir("files");
+	for (const file of files) {
+		await copyFile(`files/${file}`, `files-copy/${file}`, 0)
+	}
+}
+
 
